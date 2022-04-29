e157203187e72230d97d97a82ffa98c7c5136e99
Bar0809 <bardavid774@gmail.com>
2022-04-29 12:11:58 +0300

    update changes

---
+++ src/HomePage.jsx
@@ -0,0 +1,19 @@
+
+
+export function HomePage (){
+    // return ( <div class="home">
+    //     {/* <div class= "Toolbar">
+    //         <Link to="HomePage">דף הבית</Link>
+    //         <Link to="HabitantProject">פרויקט דיור</Link>
+    //         <Link to="AssistanceProject">פרויקט הכוונה</Link>
+    //     </div> */}
+
+
+
+
+
+
+    // </div>);
+
+    return <div>Home</div>
+}
